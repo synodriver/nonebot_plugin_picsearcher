8824cc2054e70c97bcceeb7c840c60f5f39a3b8b
synodriver <diguohuangjiajinweijun@gmail.com>
2022-11-04 18:41:50 +0000

    bump version to 0.1.9dev1

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def main():
     dis = get_dis()
     setup(
         name="nonebot-plugin-picsearcher",
-        version="0.1.8",
+        version="0.1.9dev1",
         url="https://github.com/synodriver/nonebot_plugin_picsearcher",
         packages=packages,
         keywords=["nonebot"],
